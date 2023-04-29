77a934b8859e19c1ed4b33a1062e78e92e10e353
Mohbad <subair0323@gmail.com>
2023-04-29 05:04:21 +0000

    Modified

--- app.java
+++ app.java
@@ -2,9 +2,16 @@ We support;
 developing java apps
 .net application 
 NodeJS application 
+<<<<<<< HEAD
 python application
 web application
 Bug removed
 great
 this app is excellent
 this app is excellent for all user
+=======
+Great. I hope this process is worth
+This app is working excelent
+This app is excellent for all user
+
+
